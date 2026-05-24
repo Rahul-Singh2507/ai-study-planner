e4166cc7723edd727a29160012efe7addfee7f40
Rahul Singh <rahulsinghraj2000@gmail.com>
2026-05-24 16:13:49 +0530

    api added

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -7,7 +7,7 @@ import axios from "axios"
 ========================= */
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://ai-study-planner-l5rh.onrender.com/api",
 })
 
 
